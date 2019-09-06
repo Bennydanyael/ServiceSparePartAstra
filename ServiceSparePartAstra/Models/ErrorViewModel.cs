c1115985213ca5252259b03831104b8b381c922f
bennydanyael <bennydanyael@gmail.com>
2019-09-06 14:40:25 +0700

    Create Project

---
+++ ServiceSparePartAstra/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServiceSparePartAstra.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
